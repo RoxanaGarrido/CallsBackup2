4f8ee72243e4a4dd0671806bbdc3eb687f423623
Roxana <roxana@recitecnic.es>
2021-11-09 12:04:42 +0100

    Segons formater

--- app/src/main/java/es/recitecnic/rgarrido/callsbackup2/MainActivity.kt
+++ app/src/main/java/es/recitecnic/rgarrido/callsbackup2/MainActivity.kt
@@ -7,16 +7,23 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.database.Cursor
+import android.os.Build
 import android.os.CancellationSignal
 import android.provider.CallLog
 import android.widget.ListView
 import android.widget.SimpleCursorAdapter
+import androidx.annotation.RequiresApi
 import es.recitecnic.rgarrido.callsbackup2.databinding.ActivityMainBinding
 import java.lang.String.format
 import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.*
 import java.util.function.Consumer
 
+@RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -53,7 +60,7 @@ class MainActivity : AppCompatActivity() {
             displayLog()
     }
 
-    @SuppressLint("MissingPermission")
+    @SuppressLint("MissingPermission", "SimpleDateFormat")
     private fun displayLog() {
 
         var data = arrayOf<String>(
@@ -81,14 +88,47 @@ class MainActivity : AppCompatActivity() {
             }
 
             val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
-            val simpleTimeFormat = SimpleDateFormat("HH:mm:ss")
 
             var info = "\n\nTeléfono: "+rs.getString(1)
             info+= "\nTipo: "+type
-            info+="\nDuración: "+simpleTimeFormat.format(rs.getString(3).toLong())
+            info+="\nDuración: "+formatSegons(rs.getString(3).toLong())
             info+="\nFecha: "+simpleDateFormat.format(rs.getString(4).toLong())
             binding.editTextTextMultiLine.append(info)
 
         }
     }
-}
+
+    private fun formatSegons(TotalSegundos:Long):String{
+        // define once somewhere in order to reuse it
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+       // JVM representation of a millisecond epoch absolute instant
+        val instant = Instant.ofEpochMilli(TotalSegundos)
+
+       // Adding the timezone information to be able to format it (change accordingly)
+        val date = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
+        val ret = formatter.format(date)
+        return ret
+        /*
+        var restant:Long
+        var restant1:Long
+        var restant2:Long
+        var horas:Long
+        var minutos:Long
+        var segundos:Long
+        var format:String=""
+        restant = TotalSegundos%(3600*24);
+        horas = restant / 3600;
+        restant1 = TotalSegundos % 3600;
+
+        minutos = restant1 /60;
+        restant2 = TotalSegundos % 60;
+
+        segundos = restant2;
+
+        format = "$horas:$minutos:$segundos"
+        return format
+        */
+
+    }
+ }
